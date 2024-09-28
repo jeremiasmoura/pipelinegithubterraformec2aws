557a1d9acdfdf1205d7b1e407dd62af4589cc6e8
Jeremias Barbosa de Moura <140743178+jeremiasmoura@users.noreply.github.com>
2024-09-28 10:35:47 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,9 @@
+instanceName = "minha-instancia-ec2"
+imageId = "ami-xxxxxxxx"
+instanceType = "t2.micro"
+keyName = "minha-chave"
+vpcId = "vpc-xxxxxxxx"
+securityGroupName = "meu-security-group"
+securityGroupDescription = "Security group para liberar HTTP na porta 80"
+aws_region = "us-east-1"
 
