d51890c641f0b64c6277043ec807f2196cbe0f95
Jeremias Barbosa de Moura <140743178+jeremiasmoura@users.noreply.github.com>
2024-09-28 15:24:51 +0000

    commit

--- infra/aws_ec2_instance.tf
+++ infra/aws_ec2_instance.tf
@@ -5,7 +5,6 @@ module "aws_ec2_instance" {
 
   ami                    = var.imageId
   instance_type          = var.instanceType
-  key_name               = var.keyName
   monitoring             = true
   vpc_security_group_ids = [module.aws_security_group.security_group_id,
                             aws_security_group.aws_ssh_access.id]
