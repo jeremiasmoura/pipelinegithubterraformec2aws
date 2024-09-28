99542d92e17aaa5b44b59f41fcd82678ed309fad
Jeremias Barbosa de Moura <140743178+jeremiasmoura@users.noreply.github.com>
2024-09-28 14:24:28 +0000

    commit

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,9 +1,44 @@
-variable "pipe_bucket_prefix" {
-    type = string
-    description = "Prefix to the bucket_name"
+variable "aws_region" {
+  type        = string
+  description = "This is the region where the resources will be registred"
+  default     = "us-east-2"
 }
 
-variable "pipe_region" {
-  type = string
-  description = "region to aws provider"
+variable "imageId" {
+  type        = string
+  description = "This is the image id to ec2 instances"
+}
+
+variable "instanceType" {
+  type        = string
+  description = "This is the instance type to an EC2 resource"
+}
+
+variable "keyName" {
+  type        = string
+  description = "This is the key pair name to an EC2 resource"
+}
+
+variable "instanceName" {
+  type        = string
+  description = "This is the name to an EC2 resource"
+}
+
+variable "subnetId" {
+  type        = string
+  description = "This is the subnetID to an EC2 resource"
+}
+
+variable "securityGroupName" {
+  type        = string
+  description = "This is the security group name to HTTP ingress"
+}
+
+variable "securityGrouDescription" {
+  type        = string
+  description = "This is the security group description to HTTP ingress"
+}
+
+variable "vpcId" {
+  description = "This is the public VPC ID to HTTP ingress"
 }
--- variables.tf
+++
@@ -1,9 +0,0 @@
-instanceName = "nome-ec2"
-imageId = "ami-0583d8c7a9c35822c"
-instanceType = "t2.micro"
-keyName = "minha-chave"
-vpcId = "vpc-04b0af7bdcc2231c5"
-securityGroupName = "meu-security-group"
-securityGroupDescription = "Security group para liberar HTTP na porta 80"
-aws_region = "us-east-1"
-
