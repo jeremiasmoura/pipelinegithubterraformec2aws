04d34387cd2f82930c9785013fa59402d8851899
Jeremias Barbosa de Moura <140743178+jeremiasmoura@users.noreply.github.com>
2024-09-28 10:32:03 -0300

    Update install_http.sh

--- files/install_http.sh
+++ files/install_http.sh
@@ -1,1 +1,11 @@
-
+#!/bin/bash
+yum update -y
+yum install httpd.x86_64 -y
+systemctl start httpd
+systemctl enable httpd
+echo '<!DOCTYPE html>' > /var/www/html/index.html
+echo '<html>' >> /var/www/html/index.html
+echo '<body>' >> /var/www/html/index.html
+echo '  <h1 style="color:red;">Sensacional!!!! Partiu! Sexo, Drogas e Rock and Roll</h1>' >> /var/www/html/index.html
+echo '</body>' >> /var/www/html/index.html
+echo '</html>' >> /var/www/html/index.html
